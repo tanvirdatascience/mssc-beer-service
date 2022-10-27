9c543bb016b2c1156ef343ae3df08d48620d93aa
rivnatdemha <tambir.ahmedcs@gmail.com>
2022-10-27 19:56:39 -0400

    create model

---
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
+}
